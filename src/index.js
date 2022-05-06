08ac8387449d3df41db1716e564d0e8646df7ba7
Habtamu Debele <habtlig2010@gmail.com>
2022-05-06 11:09:36 -0400

    First Try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", function(event){
+    event.preventDefault()
+  })
 });
+
+console.log("THis is performed before the DOM is loaded")
+
